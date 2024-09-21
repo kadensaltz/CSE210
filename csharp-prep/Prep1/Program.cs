83a275994dc8e2694dc10a077e943717a0bf0b56
kadensaltz <kadensaltz@gmail.com>
2024-09-21 15:59:43 -0600

    prep2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -13,7 +13,4 @@ class Program
         Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 
-
-
-
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percent grade did you recieve in the class? ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+
+        string letter = "";
+
+        if (numberGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got a(n) {letter}");
+
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("You passed, Congrats!!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, better luck next time.");
+        }
     }
 }
