a4d1bcd7cb089e65b63c11c770cb629eb7baf705
kadensaltz <kadensaltz@gmail.com>
2024-11-01 13:46:07 -0600

    completed prepare04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("John", "History");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("7.3", "8-19", "John", "Math");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("The Causes of World War II by Mary Waters", "John", "English");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
---
+++ prove/Develop04/Activities.cs
@@ -0,0 +1,17 @@
+class Activities
+{
+    protected string startingMessage;
+
+    protected string endingMessage;
+
+    public void timer()
+    {
+        
+    }
+
+    public void spinner()
+    {
+            
+    }
+    
+}
